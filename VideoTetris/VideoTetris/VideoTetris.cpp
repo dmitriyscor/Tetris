23513583d50c2eafdee4c8e2dd684f50e3986582
dmitriyscor <Dmitriyscorpio@gmail.com>
2024-03-10 20:24:46 -0700

    commit 0.2

--- VideoTetris/VideoTetris/VideoTetris.cpp
+++ VideoTetris/VideoTetris/VideoTetris.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 wstring tetmino[7];
 
+int rotate(int px, int py, int r);
+
 int main()
 {
     std::cout << "Hello World!\n";
@@ -45,4 +47,15 @@ int main()
 
 }
 
+int rotate(int px, int py, int r)
+{
+    switch (r % 4)
+    {
+    case 0: return py * 4 + px; //0 degrees
+    case 1: return 12 + py - (px*4); //90 degrees
+    case 2: return 15 - (py * 4) - px; //180 degrees
+    case 3: return 3 - py + (px * 4); //270 degrees
+    }
+    return 0;
+}
 
